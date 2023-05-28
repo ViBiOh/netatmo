6e599ab6ef2b37dcc2f9b58707c77aae28a52417
Vincent Boutour <bob@vibioh.fr>
2023-05-28 20:47:48 +0200

    refactor(flags): Updating usage
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/netatmo/netatmo.go
+++ cmd/netatmo/netatmo.go
@@ -30,6 +30,7 @@ const (
 
 func main() {
 	fs := flag.NewFlagSet("netatmo", flag.ExitOnError)
+	fs.Usage = flags.Usage(fs)
 
 	appServerConfig := server.Flags(fs, "")
 	promServerConfig := server.Flags(fs, "prometheus", flags.NewOverride("Port", uint(9090)), flags.NewOverride("IdleTimeout", 10*time.Second), flags.NewOverride("ShutdownTimeout", 5*time.Second))
